cf58a3c5a3cf54d30b22aaec29b9705a49629c95
govind <govindmh14@gmail.com>
2020-08-05 14:25:14 +0530

    Initial Commit

---
+++ MrIdli/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MrIdli
+//
+//  Created by govind on 05/08/20.
+//  Copyright © 2020 govind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
